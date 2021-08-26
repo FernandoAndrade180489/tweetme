af7b60c8bd66f3ae5bfcdfc240d10cd823fe3b92
fernandowa <fernandow.and@gmail.com>
2021-08-26 19:15:17 -0300

    Delete Tweet API View

--- tweets/views.py
+++ tweets/views.py
@@ -50,6 +50,20 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
     serializer = TweetSerializer(obj)
     return Response(serializer.data, status=200)  
 
+
+@api_view(['DELETE', 'POST'])
+@permission_classes([IsAuthenticated])
+def tweet_delete_view(request, tweet_id, *args, **kwargs):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    qs = qs.filter(user=request.user)
+    if not qs.exists():
+        return Response({"message": "You cannot delete this Tweet"}, status=401)
+    obj = qs.first()
+    obj.delete()
+    return Response({"message": "Tweet removed"}, status=200)  
+
     
     
 # Without REST_FRAMEWORK views
