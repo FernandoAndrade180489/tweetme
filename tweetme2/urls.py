0edbfbda2033f02234924a319a5f1b1b8d655115
fernandowa <fernandow.and@gmail.com>
2021-08-28 12:46:27 -0300

    Setting up Tests in Django

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -19,10 +19,8 @@ from django.urls import path, include
 from tweets.views import (
     home_view, 
     tweet_detail_view,
-    tweet_delete_view, 
     tweet_list_view,
     tweet_create_view,
-    tweet_action_view,
 )
 
 urlpatterns = [
