10b5128407f37abec030bd241cbdebdfe5aad8a9
fernandowa <fernandow.and@gmail.com>
2021-08-31 20:16:17 -0300

    Using JavaScript Modules

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -2,61 +2,9 @@ import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function loadTweets (callback) {
-  const xhr = new XMLHttpRequest() // xhr = SomeClass() -> equivalent in Python
-  const method = 'GET' // "POST"
-  const url = "http://localhost:8000/api/tweets/"
-  const responseType = "json"
-
-  xhr.responseType = responseType
-  xhr.open(method, url)
-  xhr.onload = function () {
-    callback(xhr.response, xhr.status)
-  }
-  xhr.onerror = function (e) {
-    console.log(e)
-    callback({"message":"The request was an error"}, 400)
-  }
-  xhr.send()
-}
-
-function ActionBtn(props) {
-  const {tweet, action} = props;
-  const className = props.className ? props.className : 'btn btn-primary btn-sm';
-  return action.type === 'like' ? <button className={className}> {tweet.likes} Likes</button> : null;
-}
-
-function Tweet(props) {
-  const {tweet} = props
-  const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
-  const action = {type: "like"}
-  return (
-    <div className={className}>
-      <p>{tweet.id} - {tweet.content}</p>
-      <div className='btn btn-group'>
-        <ActionBtn tweet={tweet} action={action}/> 
-        <ActionBtn tweet={tweet} action={{type: "unlike"}}/>
-      </div>
-    </div>
-  )
-}
+import {TweetsList} from './tweets'
 
 function App() {
-  const [tweets, setTweets] = useState([])
-
-  useEffect(() => {
-    // do my lookup
-    const myCallback = (response, status) => {
-      // console.log(response, status)
-      if (status === 200) {
-        setTweets(response)
-      } else {
-        alert("There was an error")
-      }
-    }
-    loadTweets(myCallback) 
-  }, [])
-
   return (
     <div className="App">
       <header className="App-header">
@@ -65,9 +13,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-          {tweets.map((item, index)=>{
-            return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`}/>
-          })}
+          <TweetsList />
         </div>
       </header>
     </div>
---
+++ tweetme2-web/src/tweets/index.js
@@ -0,0 +1,7 @@
+import { TweetsList, ActionBtn, Tweet } from "./components";
+
+export {
+    TweetsList,
+    ActionBtn,
+    Tweet
+}
