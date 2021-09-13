673443be0c629c8dfa755d54dcb374405b3ceef3
fernandowa <fernandow.and@gmail.com>
2021-09-13 17:58:58 -0300

    Follow Button Logic and Endpoint

---
+++ profiles/api/views.py
@@ -0,0 +1,40 @@
+import random
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from ..models import Profile
+
+User = get_user_model()
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
+ 
+
+@api_view(['GET', 'POST']) 
+@permission_classes([IsAuthenticated])
+def user_follow_view(request, username, *args, **kwargs):
+    me = request.user
+    other_user_qs = User.objects.filter(username=username)
+    # profile = Profile.objects.filter(user__username=username).first()
+    if not other_user_qs.exists():
+        return Response({}, status=404)
+    other = other_user_qs.first()
+    profile = other.profile
+    data = request.data or {} # If there isn's request data is equal empty dic {}
+   
+    action = data.get("action")    
+    if action == "follow":
+        profile.followers.add(me)
+    elif action == "unfollow":
+        profile.followers.remove(me)
+    else:
+        pass
+    current_followers_qs = profile.followers.all()
+    return Response({"count": current_followers_qs.count()}, status=200)
+
+    """
+    It's not necessary Serializer, because I am return only count data and not the followers of a user.
+    """
