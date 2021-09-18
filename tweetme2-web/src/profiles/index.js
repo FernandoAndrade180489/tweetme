ac7cee93820a156fd35bd8bcc6313a90d3d8cc11
fernandowa <fernandow.and@gmail.com>
2021-09-18 18:55:55 -0300

    Render Profile Badge Component

--- tweetme2-web/src/profiles/index.js
+++ tweetme2-web/src/profiles/index.js
@@ -1,5 +1,9 @@
 import { UserDisplay, UserLink, UserPicture } from "./components";
+import { ProfileBadgeComponent } from "./badge";
 
 export {
-    UserDisplay, UserLink, UserPicture
+    ProfileBadgeComponent,
+    UserDisplay, 
+    UserLink, 
+    UserPicture,
 }
